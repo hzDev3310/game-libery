ff5838effc993048500ba4e0e6fbe6a1541ae359
hamza sayari <hamzap847@gmail.com>
2023-08-05 22:21:05 +0100

    .

--- src/componets/Navbar.tsx
+++ src/componets/Navbar.tsx
@@ -15,17 +15,29 @@ const Navbar = () => {
   const { setStr, str } = useSide()
   const { setSearchText } = useGameQueryStore();
   return (
-    <HStack justifyContent={"space-between"} paddingY={"10px"} paddingX={15}>
-      <Icon cursor={'pointer'} onClick={() => {
-        if (str === 'none') setStr('block');
-        if (str === 'block') setStr('none');
-      }} boxSize={50} display={{ base: "block", lg: "none" }} as={BsList} />
-      <Box width={20}>
-        <Link to={"/"}  > <Image src={logo} boxSize={"50px"} objectFit={"cover"} /></Link>
+    <>
+      <HStack justifyContent={"space-between"} paddingY={"10px"} paddingX={15}>
+        <HStack>
+          <Icon cursor={'pointer'} onClick={() => {
+            if (str === 'none') setStr('block');
+            if (str === 'block') setStr('none');
+          }} boxSize={50} display={{ base: "block", lg: "none" }} as={BsList} />
+          <Box width={20}>
+            <Link to={"/"}  > <Image src={logo} boxSize={"50px"} objectFit={"cover"} /></Link>
+          </Box>
+        </HStack>
+        <Box width={'100%'} display={{ sm: 'none', lg: 'block' }}>
+          <Searchbar searchValue={(e) => setSearchText(e)} />
+        </Box>
+
+        <ColorModeSwitch />
+      </HStack>
+      <Box  paddingX={5} display={{ lg: 'none', sm: 'block' }}>
+
+        <Searchbar searchValue={(e) => setSearchText(e)} />
       </Box>
-      <Searchbar searchValue={(e) => setSearchText(e)} />
-      <ColorModeSwitch />
-    </HStack>
+    </>
+
   );
 };
 
